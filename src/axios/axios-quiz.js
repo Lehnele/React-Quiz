a9e4dc17da8d6b863528c3ddecf05d41d7973600
Lehnele <danil.hap@mail.ru>
2022-02-05 18:16:40 +0300

    debug auth

--- src/axios/axios-quiz.js
+++ src/axios/axios-quiz.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 export default axios.create({
-    baseURL: 'https://react-quiz-40305-default-rtdb.europe-west1.firebasedatabase.app/'
+    baseURL: 'https://react-quiz-a6112-default-rtdb.europe-west1.firebasedatabase.app/'
 })
--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import classes from './Auth.module.css';
 import Button from '../../components/UI/Button/Button';
 import Input from '../../components/UI/Input/Input';
-import is from 'is_js'
+import is from 'is_js';
+import axios from 'axios';
 
 // function validateEmail(email) {
 //     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -39,12 +40,32 @@ const Auth = () => {
         }
     })
 
-    const loginHandler = () => {
-
+    const loginHandler = async () => {
+        const authData  = {
+            email: state.formControls.email.value,
+            password: state.formControls.password.value,
+            returnSecureToken: true
+        }
+        try {
+            const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDf-9OEcpP32LMHmZMdj2zy-hPJjxlyyTw', authData)
+            console.log(response.data)
+        } catch (e) {
+            console.log(e)
+        }
     }
 
-    const registerHandler = () => {
-
+    const registerHandler = async () => {
+        const authData  = {
+            email: state.formControls.email.value,
+            password: state.formControls.password.value,
+            returnSecureToken: true
+        }
+        try {
+            const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDf-9OEcpP32LMHmZMdj2zy-hPJjxlyyTw', authData)
+            console.log(response.data)
+        } catch (e) {
+            console.log(e)
+        }
     }
 
     const submitHandler = event => {
