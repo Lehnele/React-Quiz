baddf146ce7ecffe8a5fee1460751bbd8329361d
Lehnele <danil.hap@mail.ru>
2022-02-20 15:06:02 +0300

    firebase

---
+++ build/precache-manifest.98f5639651d808eaa42e6e0d2a50d8f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a97b2f91a4bc1dc95f410b9532b41eb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40f44e9d2b1e9226f6a6",
+    "url": "/static/css/main.70fb900a.chunk.css"
+  },
+  {
+    "revision": "38f7ced2b1fcdf19f90f",
+    "url": "/static/js/2.1b7ad09b.chunk.js"
+  },
+  {
+    "revision": "b07b0ae247918a4646639211633640c2",
+    "url": "/static/js/2.1b7ad09b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c87e16df49197b77038",
+    "url": "/static/js/3.4fbcbb4b.chunk.js"
+  },
+  {
+    "revision": "40f44e9d2b1e9226f6a6",
+    "url": "/static/js/main.46180cbf.chunk.js"
+  },
+  {
+    "revision": "dcc25c5d646989906559",
+    "url": "/static/js/runtime-main.c18236c3.js"
+  }
+]);
